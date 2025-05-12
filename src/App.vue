038c7d6aca06864f5fb9f40cb0c9d57d525fb9b9
Brandon Samayoa <103422988+brandonP2@users.noreply.github.com>
2025-05-12 20:25:34 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,3 @@
 import Sidebar from '@/components/ProductList.vue'
 import ProductList from '@/components/Sidebar.vue'
 </script>
-
-<style scoped>
-/* Puedes añadir estilos de layout generales, si lo deseas */
-</style>
