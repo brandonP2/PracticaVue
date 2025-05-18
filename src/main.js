78cd66623e9a0e8fdeb6a4e825eadb7b119d3db3
Brandon Samayoa <103422988+brandonP2@users.noreply.github.com>
2025-05-18 23:46:58 +0200

    Delete src directory

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-// Importa Tailwind (y cualquier otro estilo global que quieras)
-import '@/assets/main.css'
-
-createApp(App).mount('#app')
